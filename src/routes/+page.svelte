734ef711ffc10ff94477037396be7943686247dc
Mark Fayngersh <marfay@me.com>
2023-03-19 09:50:18 -0400

    Deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,21 @@
 <div class="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
 	<div class="mx-auto max-w-3xl">
 		<div class="flex items-center gap-4 lg:gap-8">
-			<img class="h-20 w-20 rounded-full lg:h-44 lg:w-44" src="/profile.png" alt="Profile" />
-			<h1 class="text-3xl font-medium text-gray-800 dark:text-gray-200 lg:text-7xl">
+			<img
+				class="h-36 w-36 rounded-full lg:h-60 lg:w-60 lg:[image-rendering:crisp-edges]"
+				src="/profile.jpg"
+				alt="Profile"
+			/>
+			<h1 class="text-4xl font-medium text-gray-800 dark:text-gray-200 lg:text-7xl">
 				Mark<br />Fayngersh
 			</h1>
 		</div>
 		<div class="prose prose-green py-10 dark:prose-emerald lg:prose-2xl dark:text-gray-200">
 			<p>
-				Currently building products at <a href="https://engineering.careswitch.com">Careswitch</a>,
-				a home care company I cofounded with Ilya.
+				Currently building products at <a href="https://engineering.careswitch.com">Careswitch</a>.
 			</p>
-			<p>I enjoy reading (especially philosophy), making electronic music, and photography.</p>
-			<p>I irregularly organize my thoughts on various topics <a href="/posts">here</a>.</p>
+			<p>Enjoy reading, making electronic music, and photography.</p>
+			<p>Occasionally organize my thoughts on various topics <a href="/posts">here</a>.</p>
 		</div>
 	</div>
 </div>
