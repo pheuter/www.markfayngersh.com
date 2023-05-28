9ec7eac09e7e4559f69bb2fe7a0116842ec9b3dd
Mark Fayngersh <marfay@me.com>
2023-05-28 10:36:05 -0400

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="max-w-prose px-4 py-10 sm:px-6 lg:px-8">
+	<div class="mx-auto max-w-3xl border border-red-600 p-3 dark:border-red-500">
+		<h1 class="font-serif text-3xl text-red-600 dark:text-red-500">
+			Error: {$page.error?.message}
+		</h1>
+	</div>
+</div>
