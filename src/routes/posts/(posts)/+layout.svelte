07ac49fe8f7d15cf84f35a17d8b13968880d407b
Mark Fayngersh <marfay@me.com>
2023-05-28 09:53:43 -0400

    Use markdown for posts

---
+++ src/routes/posts/(posts)/+layout.svelte
@@ -0,0 +1,22 @@
+<div class="px-4 py-10 sm:px-6 lg:px-8 lg:py-24">
+	<div
+		class="prose mx-auto dark:prose-invert lg:prose-lg prose-headings:font-serif prose-headings:font-normal prose-p:font-light prose-a:font-light prose-a:text-blue-600 prose-a:no-underline hover:prose-a:underline prose-strong:font-normal dark:prose-a:text-blue-400"
+	>
+		<a class="mb-4 inline-flex items-center gap-1" href="/posts">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				class="h-5 w-5"
+			>
+				<path
+					d="M7 3.5A1.5 1.5 0 018.5 2h3.879a1.5 1.5 0 011.06.44l3.122 3.12A1.5 1.5 0 0117 6.622V12.5a1.5 1.5 0 01-1.5 1.5h-1v-3.379a3 3 0 00-.879-2.121L10.5 5.379A3 3 0 008.379 4.5H7v-1z"
+				/>
+				<path
+					d="M4.5 6A1.5 1.5 0 003 7.5v9A1.5 1.5 0 004.5 18h7a1.5 1.5 0 001.5-1.5v-5.879a1.5 1.5 0 00-.44-1.06L9.44 6.439A1.5 1.5 0 008.378 6H4.5z"
+				/>
+			</svg>
+		</a>
+		<slot />
+	</div>
+</div>
