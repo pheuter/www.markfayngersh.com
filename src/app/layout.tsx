2577ed9d29795a626b583d3aa66511870fedf955
Mark Fayngersh <marfay@me.com>
2024-07-28 09:57:54 -0400

    edge runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,8 @@ const fontSans = FontSans({
   variable: "--font-sans",
 });
 
+export const runtime = "edge";
+
 export const metadata: Metadata = {
   title: "Mark Fayngersh",
   description: "The personal website of Mark Fayngersh",
